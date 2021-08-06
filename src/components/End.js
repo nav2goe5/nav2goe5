6c6b670e05e95befed36016dfd3b911db04625ff
Kaiuby <kai.hoering@hotmail.de>
2021-08-06 15:10:52 +0200

    Build Update

--- src/components/End.js
+++ src/components/End.js
@@ -11,19 +11,21 @@ const End = (props) => {
         <header className="Startseite-header">
           <h1 className="logo">Nav2Gö</h1>
           <div id="endText">
-            <p>          
-            <br></br>Vielen Dank für die Teilnahme! </p>
-            <p>Klicken Sie jetzt bitte auf den Button, um zur Umfrage zu gelangen
-            {/* <p> Klicken Sie jetzt bitte auf den Button, um zur zweiten App-Version zu gelangen */}
-            <br></br> 
-            <br></br>
+            <p>
+              <br></br>Vielen Dank für die Teilnahme!{" "}
+            </p>
+            <p>
+              Klicken Sie jetzt bitte auf den Button, um zur Umfrage zu gelangen
+              {/* <p> Klicken Sie jetzt bitte auf den Button, um zur zweiten App-Version zu gelangen */}
+              <br></br>
+              <br></br>
             </p>
           </div>
         </header>
         <div id="containerButtonEnd">
-          <a href="https://www.survio.com/survey/d/N0D1O5W6V9B5C9V6C">
-            Umfrage starten 
-           </a>
+          <a href="https://www.survio.com/survey/d/C3K5I4C4W7L5R1J7O">
+            Umfrage starten
+          </a>
           {/* <a href="https://morza95.github.io">
             App-Version 2 starten
           </a> */}
